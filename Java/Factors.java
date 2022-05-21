c7fb042672991c4429444d032c8ee696ca019273
Abhinandan Deka <abhinandan.deka@cognizant.com>
2022-05-21 16:33:39 +0530

    Uploaded python file

--- Java/Factors.java
+++ Java/Factors.java
@@ -18,12 +18,24 @@ public class Factors {
 
     private static void generateFactors(int num) {
 
-        
-
+        int count = 0;
+                
         for(int i = 1; i <= num; i++){
             if(num % i == 0){
-                
+                ++count;
             }
         }
+        
+        int[] arr = new int[count + 1];
+
+        for (int i = 1; i < arr.length; i++) {
+            
+            if(num % i == 0){
+                arr[i] = i;
+
+                System.out.print(arr[i] + " ");
+            }
+        }
+
     }
 }
